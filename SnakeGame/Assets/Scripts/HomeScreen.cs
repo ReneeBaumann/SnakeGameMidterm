66b56bd1fc8f67237f43ef32ca76ab504a4cc4ca
ReneeBaumann <87092758+ReneeBaumann@users.noreply.github.com>
2025-03-05 21:38:02 -0500

    Home screen flicker added

--- SnakeGame/Assets/Scripts/HomeScreen.cs
+++ SnakeGame/Assets/Scripts/HomeScreen.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class HomeScreen : MonoBehaviour
 {
     public TextMeshProUGUI _homeScreenMessage;
+    public float flickerSpeed = 0.3f;
+    private bool isFlickering = true;
 
     void Start()
     {
         _homeScreenMessage.enabled = true;
-
+        StartCoroutine(FlickerText());
     }
 
     void Update()
@@ -20,4 +24,16 @@ public class HomeScreen : MonoBehaviour
         }
 
     }
+    
+    IEnumerator FlickerText()
+    {
+        while (isFlickering)
+        {
+            _homeScreenMessage.alpha = 1f;
+            yield return new WaitForSeconds(flickerSpeed);
+            _homeScreenMessage.alpha = 0f;
+            yield return new WaitForSeconds(flickerSpeed);
+        }
+    }
+    
 }
